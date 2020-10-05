77e1c40c391b59d7b1f2eb25659e2fb20d1ff332
Rohini2020 <rohiniparkhi2017@gmail.com>
2020-10-04 20:32:19 -0700

    Change in  Register Page

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-//import {CustomValidationService} from './confirm-password.service';
+import {CustomValidationService} from './confirm-password.service';
 
 @Component({
   selector: 'app-register',
@@ -11,16 +11,21 @@ export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder ) { 
+  constructor(private formBuilder: FormBuilder,private customValidation: CustomValidationService ) { 
     this.registerForm = this.formBuilder.group({
       fname:['',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]],
       lname:['',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]],
-      description:['',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]],
+      username:['',[Validators.required]],
+      bio:['',[Validators.required]],
       email:['',[Validators.required,Validators.email,Validators.pattern(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)]],
       password:['',[Validators.required]],
       confirmpassword:['',[Validators.required]],
+      linkedin:'',
       contactnumber:['',[Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]]
-    });
+    },
+      {
+        validator: [this.customValidation.confirmedValidator('password', 'confirmpassword')]
+      });
 
   }
 
